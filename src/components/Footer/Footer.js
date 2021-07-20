299a43e906935b67eaacbeec128f6af3856d362e
shrushti chouhan <chouhanshrushti23@gmail.com>
2021-07-20 13:22:40 +0530

    resolve filter ui issues

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,33 +3,33 @@ import { FaFacebookSquare } from 'react-icons/fa';
 import { FaInstagram } from 'react-icons/fa';
 import { FaYoutube } from 'react-icons/fa';
 import { FaTwitter } from 'react-icons/fa';
-import './Footer.css';
+import classes from './Footer.module.css';
 
 class Footer extends React.Component {
     render() {
         return <div className="container-fluid">
-            <div className="bg">
-                <p className="contact"> CONTACT US </p>
+            <div className={classes.bg}>
+                <p className={classes.contact}> CONTACT US </p>
                     <div className="row">
                         <div className="col-md-4 col-2"></div>
                         <div className="col-md-1 col-2">
-                            <div className="fb" ><FaFacebookSquare /></div>
+                            <div className={classes.fb} ><FaFacebookSquare /></div>
                         </div>
                         <div className="col-md-1 col-2">
-                            <div className="insta"><FaInstagram /></div>
+                            <div className={classes.insta}><FaInstagram /></div>
                         </div>
                         <div className="col-md-1 col-2">
-                            <div className="youtube"><FaYoutube /></div>
+                            <div className={classes.youtube}><FaYoutube /></div>
                         </div>
                         <div className="col-md-1 col-2">
-                            <div className="twitter"><FaTwitter /></div>
+                            <div className={classes.twitter}><FaTwitter /></div>
                         </div>
                         <div className="col-md-4 col-2"></div>
                     </div>
-                <p className="number"> Whatsapp:+91 9136091369 </p>
-                <div className="copyright-wrapper"> 
-                    <span className="copyright">Copyright @ 2021 Geolife Youth Club |</span>
-                    <span  className="by">Designed by Bugata Abhiram</span>
+                <p className={classes.number}> Whatsapp:+91 9136091369 </p>
+                <div className={classes.c1}> 
+                    <span className={classes.copyright}>Copyright @ 2021 Geolife Youth Club |</span>
+                    <span  className={classes.by}>Designed by Bugata Abhiram</span>
                 </div>
             </div>
         </div>
