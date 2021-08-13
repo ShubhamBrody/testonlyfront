c7c1a2db26b1fcaf849abf608edd97a6a7fe6516
Shubham Tiwari <st8896464352@gmail.com>
2021-08-13 08:48:23 +0530

    Events upload

--- Backend/models/events.model.js
+++ Backend/models/events.model.js
@@ -1,14 +1,9 @@
 const mongoose = require('mongoose');
 
 const adminSchema = new mongoose.Schema({
-    heading: String,
-    content: String,
-    image: {
-        data: Buffer,
-        contentType: String,
-        default: 'logo_old.png'
-    }
-
+    imagename: String,
+    title: String,
+    description: String,
 });
 
 
